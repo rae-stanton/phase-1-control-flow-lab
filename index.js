7f3d64521fe307469157fa41def523d409e1d52c
Rae Stanton <raestanton93@gmail.com>
2023-07-31 10:45:26 -0700

    control flow
    switch cases

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  let message;
+
+  switch (true) {
+    case feet <= 400:
+      message = "This one is on me!";
+      break;
+    case feet > 400 && feet <= 2000:
+      message = "That will be twenty bucks.";
+      break;
+    case feet > 2000 && feet <= 2500:
+      message = "I will gladly take your thirty bucks.";
+      break;
+    default:
+      message = "No can do.";
+  }
+
+  return message;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
